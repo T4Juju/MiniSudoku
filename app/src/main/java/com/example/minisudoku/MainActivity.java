7dda842c9cc36fd2d0f2d9adc2ef96d7bb06b40c
CatW2000 <westcatherine00@gmail.com>
2023-04-25 11:29:21 -0400

    Messed around with background color

--- app/src/main/java/com/example/minisudoku/MainActivity.java
+++ app/src/main/java/com/example/minisudoku/MainActivity.java
@@ -1,14 +1,105 @@
 package com.example.minisudoku;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+//import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private int mLightOnColor;  //background color
+    private int mLightOffColor;
 
+    public void onChangeColorClick(View view) {
+        Intent intent = new Intent(this, OptionsMenu.class);
+        mColorResultLauncher.launch(intent);
+    }
+
+
+
+
+    @Override
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLightGrid = findViewById(R.id.light_grid);
+
+        // Add the same click handler to all grid buttons
+        for (int buttonIndex = 0; buttonIndex < mLightGrid.getChildCount(); buttonIndex++) {
+            Button gridButton = (Button) mLightGrid.getChildAt(buttonIndex);
+            gridButton.setOnClickListener(this::onLightButtonClick);
+        }
+
+        mLightOnColor = ContextCompat.getColor(this, R.color.yellow);
+        mLightOffColor = ContextCompat.getColor(this, R.color.black);
+
+        mGame = new LightsOutGame();
+        startGame();
+    }
+
+    private void startGame() {
+        mGame.newGame();
+        setButtonColors();
+    }
+
+    private void onLightButtonClick(View view) {
+
+        // Find the button's row and col
+        int buttonIndex = mLightGrid.indexOfChild(view);
+        int row = buttonIndex / LightsOutGame.GRID_SIZE;
+        int col = buttonIndex % LightsOutGame.GRID_SIZE;
+
+        mGame.selectLight(row, col);
+        setButtonColors();
+
+        // Congratulate the user if the game is over
+        if (mGame.isGameOver()) {
+            Toast.makeText(this, R.string.congrats, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void setBGcolor() {
+
+        final Button colorChange = findViewById(R.id.colorBtn);
+        colorChange.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onChangeColorClick();
+            }
+        });
+
+        for (int buttonIndex = 0; buttonIndex < mLightGrid.getChildCount(); buttonIndex++) {
+            Button BGbutton = (Button) mLightGrid.getChildAt(buttonIndex);
+
+            // Find the button's row and col
+            int row = buttonIndex / LightsOutGame.GRID_SIZE;
+            int col = buttonIndex % LightsOutGame.GRID_SIZE;
+
+            if (mGame.isLightOn(row, col)) {
+                gridButton.setBackgroundColor(mLightOnColor);
+            } else {
+                gridButton.setBackgroundColor(mLightOffColor);
+            }
+        }
+    }
+
+    public void onNewGameClick(View view) {
+        startGame();
     }
 }
--- app/src/main/java/com/example/minisudoku/OptionsMenu.java
+++ app/src/main/java/com/example/minisudoku/OptionsMenu.java
@@ -1,15 +1,53 @@
 package com.example.minisudoku;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OptionsMenu extends AppCompatActivity {
+
+    public static final String EXTRA_COLOR = "com.example.minisudoku.color";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_menu);
-    }
 
+
+
+    }
+    public void onColorSelected(View view) {
+        int colorId = R.color.white;    //initially, MainActivity background will be white
+
+        if (view.getId() == R.id.radio_green) {
+            colorId = R.color.green;
+        } else if (view.getId() == R.id.radio_blue) {
+            colorId = R.color.blue;
+        } else if (view.getId() == R.id.radio_pink) {
+            colorId = R.color.pink;
+        } else if (view.getId() == R.id.radio_purple) {
+            colorId = R.color.purple_200;
+        }
+
+
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_COLOR, colorId);
+        setResult(RESULT_OK, intent);
+        finish();
+
+
+    }
 }
+
+
 
 
