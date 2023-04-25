5d6bd554470aed8a737489a2449654d779c280bf
Dannyimperial <daniel_mp_5@hotmail.com>
2023-04-25 11:27:07 -0400

    4/25 test

--- app/src/main/java/com/example/minisudoku/MainActivity.java
+++ app/src/main/java/com/example/minisudoku/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_menu);
+        setContentView(R.layout.activity_main);
 
 
 
