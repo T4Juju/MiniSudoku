2f01bcc38af87fa4800d1b660aefac3c2298f641
TJ Farley <twfarl6093@ung.edu>
2023-04-24 20:04:38 -0400

    New buttons

--- app/src/main/java/com/example/minisudoku/MainActivity.java
+++ app/src/main/java/com/example/minisudoku/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.minisudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Bundle;
+import android.widget.RelativeLayout;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    RelativeLayout relativeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
